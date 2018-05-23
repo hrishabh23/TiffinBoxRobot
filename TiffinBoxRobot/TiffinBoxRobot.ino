85c0108f55d812682ca0076f3d0abf1d60ebf883
Hrishabh <rajputhrishabh@gmail.com>
2018-05-23 14:09:22 +0530

    Added object avoiding code

--- TiffinBoxRobot/TiffinBoxRobot.ino
+++ TiffinBoxRobot/TiffinBoxRobot.ino
@@ -3,6 +3,8 @@
 int input = 0;
 int mSpeed = 255;
 char btInput[5];
+int trigPin = 13;
+int echoPin = 9;
 
 AF_DCMotor motor1(1);
 AF_DCMotor motor2(4);
@@ -18,47 +20,70 @@ void setup() {
 
 void loop() {
 
-  int index = 0;
-  while(Serial.available()>0) {
-    input = Serial.read();
-    btInput[index++] = input;
-  }
+    int index = 0;
+    while(Serial.available()>0) {
+        input = Serial.read();
+        btInput[index++] = input;
+    }
 
-  if(input=='u') {
-    motor1.run(FORWARD);
-    motor2.run(FORWARD);
-    motor1.setSpeed(mSpeed);
-    motor.setSpeed(mSpeed);
-  }
-  else if(input=='d') {
-    motor1.run(BACKWARD);
-    motor2.run(BACKWARD);
-    motor1.setSpeed(mSpeed);
-    motor.setSpeed(mSpeed);
-  }
-  else if(input=='l') {
-    motor1.run(RELEASE);
-    motor2.run(FORWARD);
-    motor1.setSpeed(mSpeed);
-    motor.setSpeed(mSpeed);
-  }
-  else if(input=='r') {
-    motor1.run(FORWARD);
-    motor2.run(RELEASE);
-    motor1.setSpeed(mSpeed);
-    motor.setSpeed(mSpeed);
-  }
-  else if(input=='A') {
-    index=0;
-    mSpeed=0;
-    while(btInput[index]!='A') {
-      mSpeed*=10;
-      mSpeed+=btInput[index]-48;
-      index++;
+    if(input=='u') {                  //moving ahead
+        motor1.run(FORWARD);
+        motor2.run(FORWARD);
+        motor1.setSpeed(mSpeed);
+        motor.setSpeed(mSpeed);
+    }
+    else if(input=='d') {             //moving backwards
+        motor1.run(BACKWARD);
+        motor2.run(BACKWARD);
+        motor1.setSpeed(mSpeed);
+        motor.setSpeed(mSpeed);
+    }
+    else if(input=='l') {             //turning left
+        motor1.run(RELEASE);
+        motor2.run(FORWARD);
+        motor1.setSpeed(mSpeed);
+        motor.setSpeed(mSpeed);
+    }
+    else if(input=='r') {             //turning right
+        motor1.run(FORWARD);
+        motor2.run(RELEASE);
+        motor1.setSpeed(mSpeed);
+        motor.setSpeed(mSpeed);
+    }
+    else if(input=='A') {             //speed control by slider
+        index=0;
+        mSpeed=0;
+        while(btInput[index]!='A') {
+            mSpeed*=10;
+            mSpeed+=btInput[index]-48;
+            index++;
+        }
+    }
+    else if(input=='o') {             //object avoiding
+        digitalWrite(trigPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(5);
+        digitalWrite(trigPin, LOW);
+
+        long duration = pulseIn(echoPin, HIGH);
+        long distance = duration/29/2;    //distance in cm
+      
+        if(distance<25) {
+            motor1.run(FORWARD);
+            motor2.run(REVERSE);
+            delayMicroseconds(500);
+        }
+        else {
+            motor1.run(FORWARD);
+            motor2.run(FORWARD);
+        }
+    }
+    else if(input=='w') {               //wall follower
+        //TODO: Code this
     }
     else {
-      motor1.run(RELEASE);
-      motor2.run(RELEASE);
+        motor1.run(RELEASE);
+        motor2.run(RELEASE);
     }
-  }
 }
